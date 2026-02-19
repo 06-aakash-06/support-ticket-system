277f0dde189004de737aa3f566ca01cbf48860d4
Aakash B <aakash@Aakashs-MacBook-Pro.local>
2026-02-19 08:35:40 +0530

    Backend complete - dockerized django + postgres + openrouter llm ticket classification functional

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ticket_db",
+        "USER": "ticket_user",
+        "PASSWORD": "ticket_pass",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
